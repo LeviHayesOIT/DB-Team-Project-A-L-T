079d7435674e0fdbe2dc4a949f971b56a7562655
Levi Hayes <levi.hayes@oit.edu>
2018-11-07 23:40:02 -0800

    Add project files.

---
+++ DB Team Project A L T/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DB_Team_Project_A_L_T.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
