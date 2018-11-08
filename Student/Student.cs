5f7fc8bcc325f1067c91d4421160caf239ed3704
LeviHayesOIT <43867060+LeviHayesOIT@users.noreply.github.com>
2018-11-08 12:05:51 -0800

    Added StudentLib

---
+++ Student/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StudentLib
+{
+    public class Student
+    {
+        int UserID { get; set; }
+
+        string PreferredName { get; set; }
+        string LastName { get; set; }
+
+    }
+}
