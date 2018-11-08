cbc9338c636d219268ea24d02e70666fb4df6836
LeviHayesOIT <43867060+LeviHayesOIT@users.noreply.github.com>
2018-11-08 12:03:16 -0800

    Added Student Model

---
+++ DB Team Project A L T/Models/StudentModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using StudentLib;
+
+namespace DB_Team_Project_A_L_T.Models
+{
+    public class StudentModel
+    {
+        public static List<Student> AllStudents;
+
+        int StudentID;
+
+        [Required(ErrorMessage ="Please Enter the First Name")]
+        [DisplayName("First Name")]
+        string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please Enter the Last Name")]
+        [DisplayName("Last Name")]
+        string LastName { get; set; }
+
+        [DisplayName("Preferred Name")]
+        string PreferredName { get; set; }
+
+        [Required(ErrorMessage = "Please Enter the Password")]
+        [DataType(DataType.Password)]
+        [DisplayName("Password")]
+        string Password { get; set; }
+
+        bool IsAdmin;
+    }
+}
